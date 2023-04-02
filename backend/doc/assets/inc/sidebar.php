0499384d83f1e1b7f493d5cf8f523201603cb17b
ewangelia <angeeliaw@gmail.com>
2023-04-02 23:37:56 +0700

    update

--- backend/doc/assets/inc/sidebar.php
+++ backend/doc/assets/inc/sidebar.php
@@ -20,18 +20,18 @@
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class="fab fa-accessible-icon "></i>
-                                    <span> Patients </span>
+                                    <span> Pasien </span>
                                     <span class="menu-arrow"></span>
                                 </a>
                                 <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
+                                    <!-- <li>
                                         <a href="doc_today_schedule.php">Jadwal Hari Ini</a>
-                                    </li>
+                                    </li> -->
                                     <li>
                                         <a href="doc_view_patients.php">Daftar Pasien</a>
                                     </li>
                                     <li>
-                                        <a href="his_doc_manage_patient.php">Manage Patients</a>
+                                        <a href="his_doc_manage_patient.php">Manage Pasien</a>
                                     </li>
                                 </ul>
                             </li>
@@ -41,57 +41,22 @@
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class="mdi mdi-pill"></i>
-                                    <span> Pharmacy </span>
+                                    <span> Obat </span>
                                     <span class="menu-arrow"></span>
                                 </a>
                                 <ul class="nav-second-level" aria-expanded="false">
                                     
-                                    <li>
-                                        <a href="his_doc_view_pharm_cat.php">View Pharm Category</a>
+                                
+                                        <a href="doc_view_obat.php">Daftar Obat</a>
                                     </li>
-                                    <li>
-                                        <a href="his_doc_view_pharmaceuticals.php">View Pharmaceuticals</a>
-                                    </li>
-                                    
-                                    <li>
-                                        <a href="his_doc_add_presc.php">Add Prescriptions</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_view_presc.php">View Prescriptions</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_manage_presc.php">Manage Prescriptions</a>
-                                    </li>
+                                
                                 </ul>
                             </li>
 
                             
                           
                 
-                            <li>
-                                <a href="javascript: void(0);">
-                                    <i class="mdi mdi-flask"></i>
-                                    <span> Laboratory </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_doc_patient_lab_test.php">Patient Lab Tests</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_patient_lab_result.php">Patient Lab Results</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_patient_lab_vitals.php">Patient Vitals</a>
-                                    </li>
-                                    
-                                    <li>
-                                        <a href="his_doc_lab_report.php">Lab Reports</a>
-                                    </li>
-                                    <hr>
-                                    
-                                </ul>
-                            </li>
+                           
 
                             
 
---
+++ backend/doc/doc_view_obat.php
@@ -0,0 +1,159 @@
+<?php
+  session_start();
+  include('assets/inc/config.php');
+  include('assets/inc/checklogin.php');
+  check_login();
+  //$aid=$_SESSION['ad_id'];
+  $id_user = $_SESSION['id_user'];
+  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    
+<?php include('assets/inc/head.php');?>
+
+    <body>
+
+        <!-- Begin page -->
+        <div id="wrapper">
+
+            <!-- Topbar Start -->
+                <?php include('assets/inc/nav.php');?>
+            <!-- end Topbar -->
+
+            <!-- ========== Left Sidebar Start ========== -->
+                <?php include("assets/inc/sidebar.php");?>
+            <!-- Left Sidebar End -->
+
+            <!-- ============================================================== -->
+            <!-- Start Page Content here -->
+            <!-- ============================================================== -->
+
+            <div class="content-page">
+                <div class="content">
+
+                    <!-- Start Content-->
+                    <div class="container-fluid">
+                        
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box">
+                                    <div class="page-title-right">
+                                        <ol class="breadcrumb m-0">
+                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
+                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Obat</a></li>
+                                            <li class="breadcrumb-item active">Daftar Obat</li>
+                                        </ol>
+                                    </div>
+                                    <h4 class="page-title">Obat-obatan</h4>
+                                </div>
+                            </div>
+                        </div>     
+                        <!-- end page title --> 
+
+                      
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card-box">
+                                    <h4 class="header-title"></h4>
+                                    <div class="mb-2">
+                                        <div class="row">
+                                            <div class="col-12 text-sm-center form-inline" >
+                                                
+                                                <div class="form-group">
+                                                    <input id="demo-foo-search" type="text" placeholder="Search" class="form-control form-control-sm" autocomplete="on">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="table-responsive">
+                                        <table id="demo-foo-filtering" class="table table-bordered toggle-circle mb-0" data-page-size="7">
+                                            <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th data-toggle="true">Nama Obat</th>
+                                                <th data-hide="phone">Kode Obat</th>
+                                                <th data-hide="phone">Stok Obat</th>
+                                                <th data-hide="phone">Satuan</th>
+                                               
+                                            </tr>
+                                            </thead>
+                                            <?php
+                                            /*
+                                                *get details of all obat
+                                                *
+                                            */
+                                                $ret="SELECT * FROM  obat ORDER BY RAND() "; 
+                                                $stmt= $mysqli->prepare($ret) ;
+                                                $stmt->execute() ;//ok
+                                                $res=$stmt->get_result();
+                                                $cnt=1;
+                                                while($row=$res->fetch_object())
+                                                {
+                                            ?>
+
+                                                <tbody>
+                                                <tr>
+                                                    <td><?php echo $cnt;?></td>
+                                                    <td><?php echo $row->nama_obat;?></td>
+                                                    <td><?php echo $row->kode_obat;?></td>                                                   
+                                                    <td><?php echo $row->stok;?></td>
+                                                    <td><?php echo $row->satuan;?></td>
+                                                    
+                                                </tr>
+                                                </tbody>
+                                            <?php  $cnt = $cnt +1 ; }?>
+                                            <tfoot>
+                                            <tr class="active">
+                                                <td colspan="8">
+                                                    <div class="text-right">
+                                                        <ul class="pagination pagination-rounded justify-content-end footable-pagination m-t-10 mb-0"></ul>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            </tfoot>
+                                        </table>
+                                    </div> <!-- end .table-responsive-->
+                                </div> <!-- end card-box -->
+                            </div> <!-- end col -->
+                        </div>
+                        <!-- end row -->
+
+                    </div> <!-- container -->
+
+                </div> <!-- content -->
+
+                <!-- Footer Start -->
+                 <?php include('assets/inc/footer.php');?>
+                <!-- end Footer -->
+
+            </div>
+
+            <!-- ============================================================== -->
+            <!-- End Page content -->
+            <!-- ============================================================== -->
+
+
+        </div>
+        <!-- END wrapper -->
+
+
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <!-- Vendor js -->
+        <script src="assets/js/vendor.min.js"></script>
+
+        <!-- Footable js -->
+        <script src="assets/libs/footable/footable.all.min.js"></script>
+
+        <!-- Init js -->
+        <script src="assets/js/pages/foo-tables.init.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/app.min.js"></script>
+        
+    </body>
+
+</html>
